16d0dbf8646f8a80d515410b342b741995ca5bf7
Hannah Krager <hannahkrager@gmail.com>
2016-03-07 18:08:13 -0800

    setup files for echo server

---
+++ src/client.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+
+"""Create sample client to test echo server functionality."""
+import sys
+
+def client(message):
+    # open socket
+    # pass msg
+    # accumulate msg to string
+    # close connection
+    # return message
+
+if __name__ == "__main__":
