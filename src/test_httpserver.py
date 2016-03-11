2d58e7d5f0f721f7fad6ba3c9cc837c8c36bfd77
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-11 15:08:02 -0800

    got another functional test

--- src/test_httpserver.py
+++ src/test_httpserver.py
@@ -6,13 +6,21 @@ ERROR_RESPONSES = [(["405", "Method not allowed"], ),
                    (["403", "Forbidden"], ),
                    (["400", "Bad Request"],)]
 
+SUCCESS_RESPONSE = ("""HTTP/1.1 200 OK\r\nContent-Type: text/plain Content-Type Length: 95\r\n\r\nThis is a very simple text file. Just to show that we can serve it up. It is three lines long.""")
+
+GOOD_REQUEST = []
+
+
+
 def test_response_ok():
     from server import response_ok
-    result = "/test/path"
-    response = response_ok("/test/path")
+    request_tuple = ("text/plain", "This is a very simple text file. Just to show that we can serve it up. It is three lines long.".encode('ascii'))
+    SUCCESS_RESPONSE = ("""HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 94\r\n\r\nThis is a very simple text file. Just to show that we can serve it up. It is three lines long.""")
+    response = response_ok(request_tuple)
     response = response.decode('utf-8')
-    split_response = response.split('\n')
-    assert result == split_response[3]
+    split_response = response.split('\r\n')
+    split_success = SUCCESS_RESPONSE.split('\r\n')
+    assert split_response[1] == split_success[1]
 
 def test_response_error():
     from server import response_error
@@ -54,13 +62,13 @@ def test_parse_method_2():
         parse_request(argument)
 
 
-def test_good_request():
-    """Test that server returns appropriate response to a good request"""
-    from server import parse_request
-    request = "GET /webroot/sample.txt HTTP/1.1\r\nHost: www.mysite1.com:80\r\n\r\n"
-    response = parse_request(request)
-    response = response.decode('utf-8').split('\n')
-    assert '/webroot/sample.txt' == response[3]
+# def test_good_request():
+#     """Test that server returns appropriate response to a good request"""
+#     from server import parse_request
+#     request = "GET /webroot/sample.txt HTTP/1.1\r\nHost: www.mysite1.com:80\r\n\r\n"
+#     response = parse_request(request)
+#     response = response.decode('utf-8').split('\n')
+#     assert '/webroot/sample.txt' == response[3]
 
 
 def test_resolve_uri():
